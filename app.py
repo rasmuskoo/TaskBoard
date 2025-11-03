cea2909975528ac3029c0e7e119d3e13d1555494
Rasmus Kaukonen <rasmus.kaukonen@gmail.com>
2025-11-03 12:28:15 +0200

    User registration feature and page added

---
+++ app.py
@@ -0,0 +1,29 @@
+import sqlite3
+from flask import Flask
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import config
+import db
+
+app = Flask(__name__)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät täsmää"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo olemassa"
+
+    return "Tunnus luotu onnistuneesti"
